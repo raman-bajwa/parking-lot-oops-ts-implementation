0514559215866d1e9f409870c0cde258f9aa4f0b
Raman bajwa <Ramanbajwa197@gmail.com>
2021-10-03 14:21:37 +0530

    Implemented Park Class

---
+++ classes/parking.ts
@@ -0,0 +1,37 @@
+import { Slot } from './slot';
+import { Car } from './car';
+
+class Parking {
+    slots: Slot[];
+    capacity: number;
+    parkedCars: number;
+
+    constructor(length: number){
+        this.slots = [];
+
+        for(let i=0; i < length; i++){
+            this.slots[i] = new Slot(i + 1);
+        }
+
+        this.capacity = length;
+        this.parkedCars = 0;
+    }
+
+    parkCar(slotNumber: number, car: Car): void{
+        this.slots[slotNumber - 1].setCar(car);
+
+        this.parkedCars++;
+    }
+
+    removeCar(slotNumber: number): void{
+        this.slots[slotNumber - 1].removeCar();
+    }
+
+    getNumberOfParkedCars(): number{
+        return this.parkedCars;
+    }
+
+    getNumberOfAvailbleSlots(): number{
+        return this.capacity - this.parkedCars;
+    }
+}
