7fc253c2e18f01f4091891147529285c0d66feaf
Raman bajwa <Ramanbajwa197@gmail.com>
2022-01-03 15:39:14 +0530

    Feat: Add test cases to car

--- tests/car.test.ts
+++ tests/car.test.ts
@@ -1,10 +1,11 @@
-
 import { Car } from '../src/classes/car';
 
 describe('Create Car', () => {
     it('Car have all the properities defined', () => {
         const car = new Car("JH 40B 1762", "2007");
         
+        expect(car).toBeDefined();
+        expect(car).toBeInstanceOf(Car);
         expect(car.regsitrationNumber).toBe("JH 40B 1762")
         expect(car.model).toBe("2007")
     })
